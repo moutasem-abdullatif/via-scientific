a20b018400d967d52c825a08ab408dc6fd0bc580
Moutasem <moutasem.m.abdullatif@gmail.com>
2023-12-18 16:37:19 +0300

    small changes "won't be used"

--- mongoDB/db-seed.sh
+++ mongoDB/db-seed.sh
@@ -1,11 +1,11 @@
 # Start MongoDB in the background
-mongod --fork --logpath /var/log/mongod.log
+# mongod --fork --logpath /var/log/mongod.log
 
 # Wait for MongoDB to start
-sleep 5
+# sleep 5
 
 # Import the JSON file
-mongoimport --db viaScientificDB --collection genomics --file ./data/db.json --jsonArray 
+# mongoimport --db viaScientificDB --collection genomics --file ./db.json --jsonArray 
 
 # Keep the container running
-tail -f /dev/null
+# tail -f /dev/null
